8da3f5fdcd6531838fa6bdbb1041c9840b9a10b9
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2012-12-15 18:04:13 +0200

    Scrabble table design and MySQL words database

---
+++ public/js/table.js
@@ -0,0 +1,10 @@
+
+var table_size = 15;
+
+for(var i = 0; i < table_size; i++){ 
+	document.write("<tr>");
+	for(var j = 0; j < table_size; j++){
+		document.write("<td></td>");
+	}
+	document.write("</tr>");
+}
