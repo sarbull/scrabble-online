da29a432d83be3f3f4c20c414c403392d922a214
Bogdan-Ciprian Bresug <bogdan.bresug@gmail.com>
2013-01-12 13:21:17 +0200

    Improved do colors

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,47 +1,30 @@
 function do_colors(){
-			//Culoarea roz
 
 			var random_number = Math.floor(Math.random()*2);
-			if(random_number == 1){
+
+			if(random_number == 1)
 				style="background-image:url(images/darker.png)";
-			} else {
+			else
 				style="background-image:url(images/lighter.png)";
-			}
 
-			if((i == 2 && j == 2) || (i == 3 && j == 3) || (i == 4 && j == 4) || (i == 5 && j == 5)
-				|| (i == 11 && j == 11) || (i == 12 && j == 12) || (i == 13 && j == 13) || (i == 14 && j == 14)
-				|| (i == 2 && j == 14) || (i == 3 && j == 13) || (i == 4 && j == 12) || (i == 5 && j == 11)
-				|| (i == 14 && j == 2) || (i == 13 && j == 3) || (i == 12 && j == 4) || (i == 11 && j == 5)){
+			//Culoarea roz
+			if((i == j || (i+j) == 16) && (( 2 <= i && i <=5) || 11 <= i && i <= 14))
 				style = 'background-color:#f4cecd';
-			}
-
+			
 			//Culoarea albastra
-			if((i == 2 && j == 6) || (i == 2 && j == 10) ||
-				(i == 6 && j == 2) || (i == 6 && j == 6) || (i == 6 && j == 10) || (i == 6 && j == 14) ||
-				(i == 10 && j == 2) || (i == 10 && j == 6) || (i == 10 && j == 10) || (i == 10 && j == 14) ||
-				(i == 14 && j == 6) || (i == 14 && j == 10)){
-					style = 'background-color:#66c9e8';
-			}
+			if (((i == 6 || i == 10) && (j%4 == 2)) || ((i == 2 || i == 14) && (j == 6 || j == 10)))
+				style = 'background-color:#66c9e8';
 
 			//Culoarea rosie
-			if((i == 1 && j == 1) || (i == 1 && j == 8) || (i == 1 && j == 15) || (i == 8 && j == 1) || (i == 15 && j == 1) || (i == 8 && j == 15) || (i == 15 && j == 8) || (i == 15 && j == 15)){
+			if (i%7 == 1 && j%7 ==1)
 				style = 'background-color:#ee3940';
-			}
 
-			//Culoarea verde #b8cc69
-			if(
-				(i == 1 && j == 4) || (i == 1 && j == 12) 
-				|| (i == 3 && j == 7) || (i == 3 && j == 9) 
-				|| (i == 4 && j == 8) 
-				|| (i == 4 && j == 1) || (i == 4 && j == 15) 
-				|| (i == 7 && j == 3) || (i == 8 && j == 4) || (i == 9 && j == 3) 
-				|| (i == 7 && j == 7) || (i == 7 && j == 9) 
-				|| (i == 9 && j == 7) || (i == 9 && j == 9) 
-				|| (i == 7 && j == 13) || (i == 9 && j == 13) || (i == 8 && j == 12) 
-				|| (i == 12 && j == 1) || (i == 12 && j == 15) 
-				|| (i == 12 && j == 8) || (i == 13 && j == 7) || (i == 13 && j == 9)
-				|| (i == 15 && j == 4)  || (i == 15 && j == 12) 
-			){
-				style = 'background-color:#b8cc69';
-			}
+			//Setare culoare centrala
+			if (i == 8 && j == 8)
+				style = 'background-image:url(images/darker.png)';
+
+			//Culoare verde
+			if ((i%14 == 1 && j%8 == 4) || (i%10 == 3 && (j == 7 || j == 9)) || (i%8 == 4 && j%7 == 1)
+				|| ((i == 7 || i ==9) && (j == 3 || j == 7 || j == 9 || j ==13)) || (i==8 && j%8 == 4))
+				style = 'background-color:#b8cc69'; 
     	}
